ba88708877b7af150d47a8ef6210b5c1fe82943d
Rok Fajfar <snewix7@gmail.com>
2017-08-15 09:07:24 +0200

    shows board, updates, restarts, todo: lock @ 1 player, show player in turn

--- wsxoc.lua
+++ wsxoc.lua
@@ -2,10 +2,12 @@
 -- tic tac toe client
 
 local websocket = require 'http.websocket'
-local fx = require 'termfx'
+local tfx = require 'termfx'
+local cqueues = require 'cqueues'
+local serpent = require 'serpent'
 
 local ws = websocket.new_from_uri("ws://127.0.0.1:4000")
-ws:connect(); print('connected')
+print('connecting:',ws:connect())
 
 local playnum = 0
 
@@ -16,11 +18,52 @@ else
   error(toe)
 end
 
-while true do
-  local text = io.read("*line")
-  if text == "q" then break end
-  ws:send(text)
-  print('received:',ws:receive())
-end
+tfx.init()
+tfx.clear(tfx.color.WHITE, tfx.color.BLACK)
 
+local quit = false
+
+local cq = cqueues.new()
+
+local recv, toe
+
+cq:wrap(function ()
+  repeat
+    recv, toe = ws:receive()
+  until quit
+end)
+
+repeat
+  local evt = tfx.pollevent(100)
+  cq:step(0)
+  if recv then
+    local mupd = {string.match(recv, 'update{(%d):(%d):(%d):(%d):(%d):(%d):(%d):(%d):(%d)}')}
+    tfx.setcell(1,1,mupd[1])
+    tfx.setcell(2,1,mupd[2])
+    tfx.setcell(3,1,mupd[3])
+    tfx.setcell(1,2,mupd[4])
+    tfx.setcell(2,2,mupd[5])
+    tfx.setcell(3,2,mupd[6])
+    tfx.setcell(1,3,mupd[7])
+    tfx.setcell(2,3,mupd[8])
+    tfx.setcell(3,3,mupd[9])
+  else
+    tfx.printat(1,11,toe)
+  end
+  if evt then
+    if evt.type == 'key' then
+      if evt.char and string.match(evt.char, "[1-9]") then
+        ws:send('put@' .. evt.char .. ':' .. playnum)
+      elseif evt.char == 'r' then
+        ws:send('restart:' .. playnum)
+      end
+      quit = evt.key == tfx.key.ESC
+    end
+  end
+  tfx.printat(tfx.width() - 10, 2, "player " .. playnum)
+  tfx.present()
+until quit
+
+ws:send('quit ' .. playnum)
 ws:close()
+tfx.shutdown()
--- wsxos.lua
+++ wsxos.lua
@@ -16,7 +16,9 @@ cq:wrap(function ()
       0,0,0,
       0,0,0
     },
-    sockets = {}
+    sockets = {},
+    restart = {false, false},
+    turn = 1,
   }
   setmetatable(state.fields, {
     __tostring = function (f)
@@ -33,26 +35,47 @@ cq:wrap(function ()
     host = '127.0.0.1',
     port = 4000,
     onstream = function (sv,st)
+      local function send_updates()
+        for client_num,wsock in ipairs(state.sockets) do
+          local msg = "update" .. tostring(state.fields)
+          print('sending to', client_num, msg)
+          wsock:send(msg)
+        end
+      end
+
       local ws = websocket.new_from_stream(st, st:get_headers())
       ws:accept(); print('accepted')
       state.pc = state.pc + 1
       local player = state.pc
       state.sockets[player] = ws
       ws:send('welcome ' .. player)
+
+      send_updates() -- show board on both sides
+
       while true do
         local text, _ = ws:receive()
         if text == nil then break end
         local mput = {string.match(text,"put@(%d):(%d)")}
+        local mres = {string.match(text,"restart:(%d)")}
 
         if mput[1] then
           -- update fields
-          state.fields[tonumber(mput[1])] = tonumber(mput[2])
-
-          -- send back updated to both clients
-          for client_num,wsock in ipairs(state.sockets) do
-            print('sending to', client_num)
-            -- wsock:send("update" .. tostring(state.fields))
-            wsock:send("update" .. tostring(state.fields))
+          print('put log',mput[1],mput[2])
+          if tonumber(mput[2]) == state.turn then
+            state.fields[tonumber(mput[1])] = tonumber(mput[2])
+            -- send back updated to both clients
+            send_updates()
+            -- switch player in turn
+            if state.turn == 1 then state.turn = 2 else state.turn = 1 end
+          end
+        elseif mres[1] then
+          state.restart[tonumber(mres[1])] = true
+          if state.restart[1] and state.restart[2] then
+            -- have to restart the game
+            for i,_ in ipairs(state.fields) do state.fields[i] = 0 end
+            state.restart = {false,false}
+            state.turn = 1
+            send_updates()
           end
         else
           print(text, 'not matching any pattern')
